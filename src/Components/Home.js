6387afde8fe490be14a97d8edf33ef664b3341b6
ag-indu <indulekhaag@gmail.com>
2025-07-04 20:30:20 +0530

    update home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import CodingImage from '../assets/undraw_typing-code_6t2b.svg';
 
 const Home = () => {
   return (
     <div className='home'>
-      Hi There!
-      I'm <span>Indulekha</span>
+      <img src={CodingImage} alt="Typing code" className='home-img' />
+      <h1>
+        Hi There! <br /> I'm <span className="highlighted-name">Indulekha</span>
+      </h1>
     </div>
   )
 }

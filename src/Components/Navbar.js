16b44f2e338e5b757c08ae011d15593d243a6edb
ag-indu <indulekhaag@gmail.com>
2025-07-04 17:19:59 +0530

    Initialise skeleton navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='Navbar'>
+      <nav className='navbar-container'>
+        <ul className='nav-links'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Resume</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
